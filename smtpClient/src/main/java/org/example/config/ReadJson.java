37766a0f14b7f772f6ef2bffdbc3a360736b11b2
nathan <nathanarmand@hotmail.com>
2023-11-09 12:36:34 +0100

    gson working

--- smtpClient/src/main/java/org/example/config/ReadJson.java
+++ smtpClient/src/main/java/org/example/config/ReadJson.java
@@ -5,9 +5,13 @@ import java.io.FileReader;
 public class ReadJson {
     public Object read(String pathToJson){
         try(FileReader reader = new FileReader(pathToJson)){
+        Gson gson = new Gson();
+        Object obj = gson.fromJson(reader, Object.class);
 
+        return obj;
         }catch(Exception e){
             System.out.println("Error while reading file");
         }
+        return null;
     }
 }
