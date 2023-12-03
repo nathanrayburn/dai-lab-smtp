3166767ee3e6c05e32e6d4b06a40b6b9abf1653a
Nathan Rayburn <nathanrayburn2001@gmail.com>
2023-12-03 14:17:40 +0100

    implementation of console

--- smtpClient/src/main/java/dai/Main.java
+++ smtpClient/src/main/java/dai/Main.java
@@ -11,9 +11,21 @@ import java.util.List;
 import dai.model.Email;
 
 public class Main {
+    public static void exitWithError(String message) {
+        System.out.println(message);
+        System.exit(1);
+    }
     public static void main(String[] args) {
+
+        if(args.length != 1) {
+            System.out.println();
+            exitWithError("Error: the first parameter must be the config file. Usage: java -jar smtpClient.jar <config_file>");
+        }
+
         try {
-            Configuration config = new Configuration("src/main/java/dai/config/config.json");
+
+            Configuration config = new Configuration(args[0]);
+
             SMTPClient client = new SMTPClient(config.getSmtpHost(), config.getSmtpPort());
 
             List<Group> groups = Group.createGroups(config.getMinNumberOfEmailsPerGroup(), config.getMaxNumberOfEmailsPerGroup(), config.getNumberOfGroups(), config.getVictims(), config.getMessages());
@@ -22,8 +34,6 @@ public class Main {
 
             client.connect();
 
-           // client.sendEmails(groups, config.getMessages());
-
             // Envoyer un e-mail à chaque groupe
             for (Email e : emailList ) {
                 client.sendGroupEmail(e);
@@ -31,7 +41,7 @@ public class Main {
 
             client.close();
         }catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+            exitWithError("Error: " + e.getMessage());
         }
     }
 }
